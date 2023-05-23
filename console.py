d04b55b15af38bcfdf5b4fd2b8b4474e58cf4475
Omar Aglan <Omar.aglan91@gmail.com>
2023-05-23 06:40:39 +0200

    Tasks
    More Fixing
    
    Fixing

--- console.py
+++ console.py
@@ -146,12 +146,12 @@ class HBNBCommand(cmd.Cmd):
             if words[0] not in storage.classes():
                 print("** class doesn't exist **")
             else:
-                nl = [str(obj) for key, obj in storage.all().items()
-                      if type(obj).__name__ == words[0]]
-                print(nl)
+                l = [str(obj) for key, obj in storage.all().items()
+                     if type(obj).__name__ == words[0]]
+                print(l)
         else:
-            new_list = [str(obj) for key, obj in storage.all().items()]
-            print(new_list)
+            l = [str(obj) for key, obj in storage.all().items()]
+            print(l)
 
     def do_count(self, line):
         """Counts the instances of a class.
--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-"""Initializes the package"""
+"""Module for FileStorage autoinit."""
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
